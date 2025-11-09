c947289e8944e0cf2ab4efaccfa988ba54bab413
rainfall-lalit <lalit.wani@rainfall.one>
2025-11-09 11:23:46 +0530

    Initial clean commit

---
+++ index.js
@@ -0,0 +1,22 @@
+//node --watch server.js
+const express = require("express");
+const app = express();
+const PORT = 3000; // you can change this
+
+const now = new Date();
+//console.log(now); // full date + time
+
+// Simple GET API
+app.get("/", (req, res) => {
+  res.send(`Hello from Node.js API! Current time is:+ ${now}`)
+});
+
+// Another example route
+app.get("/user", (req, res) => {
+  res.json({ name: "Lalit", role: "Developer" });
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
